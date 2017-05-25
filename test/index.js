bc908168de28d4ff99af41b17c47a07faff3529a
Антон <gsaper@gmail.com>
2017-05-25 23:30:32 +0300

    run tests on source instead of transpiled code

--- test/index.js
+++ test/index.js
@@ -1,14 +1,4 @@
-import lightflowBrowser from '../dist/lightflow.js';
-import lightflowBrowserMin from '../dist/lightflow.min.js';
-
-import lightflowNodeLTS from '../lib/lts';
-import lightflowNodeOld from '../lib/0.x';
-import lightflowNode from '../lib';
-
+import lightflow from '../src/lightflow';
 import test from './test';
 
-test('browser', lightflowBrowser);
-test('browser-min', lightflowBrowserMin);
-test('node-lts', lightflowNodeLTS);
-test('node-0.x', lightflowNodeOld);
-test('node', lightflowNode);
+test('node', lightflow);
